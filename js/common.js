bf833b07d54a2108163312b1c0e3b5d2170a77f2
Skeev_ <derkach94@gmail.com>
2018-04-03 17:47:45 +0300

    added hamburger, fixed adaptive layout up to 576px

--- js/common.js
+++ js/common.js
@@ -1,12 +1,34 @@
 $(function () {
-
-    // Custom JS
+// Custom JS
+    //slick-slider
     $('.slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 9000,
+        autoplaySpeed: 6000,
+    });
+    var $page = $('html, body');
+    //smooth scroll anchor
+    $('a[href*="#"]').click(function () {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+    //hamburger
+    var $hamburger = $(".hamburger");
+    $hamburger.on("click", function(e) {
+        $hamburger.toggleClass("is-active");
+        // Do something else, like open/close menu
+        if ($(".mobile_menu").css('display') == 'none') 
+        { 
+            $(".mobile_menu").animate({height: 'show'}, 500); 
+        } 
+        else 
+        {     
+            $(".mobile_menu").animate({height: 'hide'}, 500); 
+        } 
     });
 });
